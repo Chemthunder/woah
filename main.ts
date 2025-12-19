761f364c2eb367d59472fc8c59372a8481a6d3d8
Chemthunder <chemthunderisme@gmail.com>
2025-12-19 12:11:33 -0500

    added timer and ending :3

--- main.ts
+++ main.ts
@@ -6,7 +6,9 @@ namespace SpriteKind {
     export let VisualEffect = SpriteKind.create();
 }
 let gameTicks = 50;
+let ticks = 0;
 let isDashing = false;
+let gameActive = true;
 let playerSpriteImage = img`
     1 1 1 1 1
     1 . . . 1
@@ -35,6 +37,9 @@ let enemySpriteImage = img`
     2 . . . . 2
     2 2 2 2 2 2
 `
+let timerImg = img`
+    .
+`
 
 // anim frames
 let dash1 = img`
@@ -48,6 +53,21 @@ let dash2 = img`
     . 1 .
 `
 
+let playerMoving = img`
+    . . . . .
+    . 1 1 1 .
+    . 1 1 1 .
+    . 1 1 1 .
+    . . . . .
+`
+let playerStoppedMoving = img`
+    1 1 1 1 1
+    1 . . . 1
+    1 . . . 1
+    1 . . . 1
+    1 1 1 1 1
+`
+
 // ticking
 forever(function () {
     if (gameTicks != 0) {
@@ -55,11 +75,18 @@ forever(function () {
     } else {
         resetAndSpawn();
     }
-})
+
+    if (gameActive) {
+        ticks++;
+
+    }
+    timer.sayText(ticks, 90, false, 1, 15);
+});
 
 
 // sprites
 let player = sprites.create(playerSpriteImage, SpriteKind.Player);
+let timer = sprites.create(timerImg, SpriteKind.GuiElement)
 
 
 // constants
@@ -71,7 +98,10 @@ info.setLife(3);
 controller.moveSprite(player, 100, 100);
 player.setFlag(SpriteFlag.StayInScreen, true);
 
+timer.setPosition(screen.width / 2, screen.height - 100);
 
+timer.z = 1
+player.z = 2
 // functions
 function dash() {
     controller.moveSprite(player, 200, 200);
@@ -98,13 +128,16 @@ function dash() {
 }
 
 function resetAndSpawn() {
-    gameTicks = randint(10,300);
+    if (gameActive) {
+        gameTicks = randint(10, 300);
 
-    let enemy = sprites.create(enemySpriteImage, SpriteKind.Enemy);
-    enemy.setPosition(player.x + randint(-100, 100), player.y + randint(-100, 100));
-    enemy.follow(player, randint(10, 99));
-    enemy.setFlag(SpriteFlag.GhostThroughSprites, false);
-    info.changeScoreBy(1);
+        let enemy = sprites.create(enemySpriteImage, SpriteKind.Enemy);
+        enemy.setPosition(player.x + randint(-100, 100), player.y + randint(-100, 100));
+        enemy.follow(player, randint(10, 99));
+        enemy.setFlag(SpriteFlag.GhostThroughSprites, false);
+        info.changeScoreBy(1);
+        enemy.z = 2
+    }
 }
 
 // events
@@ -131,4 +164,49 @@ info.onLifeZero(function () {
 
     pause(250);
     sprites.destroy(player, effects.ashes, 100);
+    gameActive = false;
+    info.setScore(0);
+
 })
+
+info.onScore(1, function () {
+    gameActive = false;
+    controller.moveSprite(player, 0, 0)
+    player.vx = 0;
+    player.vy = 0;
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy, effects.disintegrate);
+
+    animation.runMovementAnimation(
+        player,
+        animation.animationPresets(animation.flyToCenter),
+        2000, false);
+
+    animation.stopAnimation(animation.AnimationTypes.All, timer)
+    animation.runMovementAnimation(timer,
+        animation.animationPresets(animation.easeDown),
+        2000, false)
+
+    
+})
+
+
+// player anims
+if (gameActive = true) {
+    characterAnimations.loopFrames(
+        player,
+        [
+            playerMoving
+        ],
+        10,
+        characterAnimations.rule(Predicate.Moving));
+
+    characterAnimations.loopFrames(
+        player,
+        [
+            playerStoppedMoving
+        ],
+        10,
+        characterAnimations.rule(Predicate.NotMoving));
+}
+animation.runMovementAnimation(timer,
+    animation.animationPresets(animation.bobbing), 2000, true)
